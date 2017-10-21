31cbfe23599da42ec81eff44713bc636f4c320b8
Matt English <menglish2451@gmail.com>
2017-10-20 20:19:33 -0700

    Move code around

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-var RPG = RPG || {};
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var game = new Phaser.Game(320, 320, Phaser.CANVAS);
-game.state.add("BootState", new RPG.BootState());
-game.state.add("LoadingState", new RPG.LoadingState());
-game.state.add("WorldState", new RPG.WorldState());
-game.state.add("BattleState", new RPG.BattleState());
-game.state.start("BootState", true, false, "assets/levels/level1.json", "WorldState", {});
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
